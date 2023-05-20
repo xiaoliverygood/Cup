a72fa263f2ed2556fb72326a37180c3a6781ae3a
L <123456@qq.com>
2023-05-20 20:55:09 +0800

    基本框架搭建

--- src/main/java/com/example/service/StudentService.java
+++ src/main/java/com/example/service/StudentService.java
@@ -3,6 +3,7 @@ package com.example.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.common.BaseResponse;
 import com.example.model.dto.RegisterStudentDTO;
+import com.example.model.entity.Student;
 import org.springframework.web.bind.annotation.RequestBody;
 
 /**
--- src/main/java/com/example/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/service/impl/StudentServiceImpl.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.common.BaseResponse;
 import com.example.mapper.StudentMapper;
 import com.example.model.dto.RegisterStudentDTO;
+import com.example.model.entity.Student;
 import com.example.service.StudentService;
 import com.example.utils.BeanCopyUtils;
 import org.springframework.stereotype.Service;
@@ -19,7 +20,9 @@ public class StudentServiceImpl extends ServiceImpl<StudentMapper, Student> impl
 
     @Override
     public BaseResponse register(RegisterStudentDTO registerStudentDTO) {
-        if(registerStudentDTO.getEmail())
+        if(registerStudentDTO.getEmail()){
+
+        }
 
         Student student = BeanCopyUtils.copyBean(registerStudentDTO, Student.class);
 
