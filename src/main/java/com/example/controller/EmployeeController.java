064ae01fd0926d3738ff63ebca3cf8c5e0232a63
L <123456@qq.com>
2023-05-24 13:26:19 +0800

    添加接口注释

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -17,25 +17,57 @@ public class EmployeeController {
 
     @Autowired
     CourseService courseService;
+
+    /**
+     * 辅导员教职工注册
+     * @param registerEmployeeDTO
+     * @return
+     */
     @PostMapping("/register")
     public BaseResponse register(@RequestBody RegisterEmployeeDTO registerEmployeeDTO){
         return employeeService.register(registerEmployeeDTO);
     }
+
+    /**
+     * 教职工登录
+     * @param loginEmployeeDTO
+     * @return
+     */
     @PostMapping("/login")
     public BaseResponse login(@RequestBody LoginEmployeeDTO loginEmployeeDTO){
         return employeeService.login(loginEmployeeDTO);
     }
+
+    /**
+     * 展示我的个人信息（教职工）
+     * @param httpServletRequest
+     * @return
+     */
     @GetMapping("/showMyMessage")
     public BaseResponse showMyMessage(HttpServletRequest httpServletRequest){
         return employeeService.showMyMessage(httpServletRequest);
     }
 //    ----------------------------辅导员的功能---------------------------------
     //修改学生档案信息
+
+    /**
+     * 修改学生信息，迎新工作辅导员的功能
+     * @param httpServletRequest
+     * @param updateStudentMessageDTO
+     * @return
+     */
     @PutMapping("/updateStudentMessage")
     public BaseResponse updateStudentMessage(HttpServletRequest httpServletRequest,@RequestBody UpdateStudentMessageDTO updateStudentMessageDTO){
         return employeeService.updateStudentMessage(httpServletRequest,updateStudentMessageDTO);
     }
     //查询某学生成绩
+
+    /**
+     * 查询某学生的成绩
+     * @param httpServletRequest
+     * @param studentId
+     * @return
+     */
 
     @GetMapping("/showStudentScore")
     public BaseResponse showStudentScore(HttpServletRequest httpServletRequest, Integer studentId){
@@ -45,12 +77,18 @@ public class EmployeeController {
     找回密码
      */
 
+    /**
+     * 找回密码(教职工忘记密码)
+     * @param findPasswordDTO
+     * @return
+     */
+
     @PutMapping("/findPassword")
     public BaseResponse findPassword(@RequestBody FindPasswordDTO findPasswordDTO){
         return employeeService.findPassword(findPasswordDTO);
     }
-    /*
-    查询我学生名单
+    /**
+    查询我的学生（我所带的学生花名册）
      */
     @GetMapping("/findMyStudent")
     public BaseResponse findMyStudent(HttpServletRequest httpServletRequest){
@@ -58,7 +96,7 @@ public class EmployeeController {
         return employeeService.findMyStudent(httpServletRequest);
     }
 
-    /*
+    /**
     通过学生id导入课程（使用课程id）
      */
     @PostMapping("ImportCourses")
@@ -66,7 +104,7 @@ public class EmployeeController {
         return employeeService.importCoursesByStudentId(httpServletRequest,studentId,courseId);
     }
 
-    /*
+    /**
     查看所有课程
      */
 
@@ -77,8 +115,8 @@ public class EmployeeController {
 //
 
 //    ----------------------宿舍管理员-----------------------------
-    /*
-    宿舍报修：修改宿舍表保修字段
+    /**
+    宿舍报修：修改宿舍表保修字段（宿舍管理员）
      */
     @PutMapping("/repair")
     public BaseResponse repair(HttpServletRequest httpServletRequest,@RequestBody RepairSupervisorDTO repairEmployeeDTO){
@@ -86,8 +124,8 @@ public class EmployeeController {
         return employeeService.repair(httpServletRequest,repairEmployeeDTO);
     }
 
-    /*
-    违纪登记：宿舍表违纪字段
+    /**
+    违纪登记：宿舍表违纪字段（宿舍管理员）
      */
     @PutMapping("/violation")
     public BaseResponse violation(HttpServletRequest httpServletRequest,@RequestBody ViolationSupervisorDTO violationEmployeeDTO){
@@ -95,8 +133,8 @@ public class EmployeeController {
         return employeeService.violation(httpServletRequest,violationEmployeeDTO);
     }
 
-    /*
-    查看学生住宿信息
+    /**
+    查看学生住宿信息（宿舍管理员）
      */
 //
     @GetMapping("/getDormitory")
@@ -106,8 +144,8 @@ public class EmployeeController {
 
     }
 
-    /*
-    查看留校人员
+    /**
+    查看留校人员（宿舍管理员）
      */
     @GetMapping("/getLeave")
     public BaseResponse getLeaveAllStudent(HttpServletRequest httpServletRequest){
@@ -117,8 +155,8 @@ public class EmployeeController {
     }
 //    ----------------------院系负责人----------------------------
 
-    /*
-    给辅导员评分
+    /**
+    给辅导员评分（院系负责人）
      */
     @PutMapping("/scoreCounselor")
     public BaseResponse scoreCounselor(HttpServletRequest httpServletRequest,Integer counselorId,Double score){
@@ -126,8 +164,8 @@ public class EmployeeController {
         return employeeService.scoreCounselor(httpServletRequest,counselorId,score);
     }
 
-    /*
-    添加科目
+    /**
+    添加科目（院系负责人）
      */
     @PostMapping("/addCourse")
     public BaseResponse addCourse(HttpServletRequest httpServletRequest,String courseName){
@@ -136,26 +174,60 @@ public class EmployeeController {
     }
 
     //----------------------学工管理器-----------------------------
+
+    /**
+     * 学生处管理：通过学生id查询学生
+     * @param id
+     * @param httpServletRequest
+     * @return
+     */
     @GetMapping("/queryStudentById")
     public BaseResponse queryStudent(Integer id , HttpServletRequest httpServletRequest){
         return employeeService.queryStudentById(id);
     }
+
+    /**
+     * 学生处管理：通过学生id修改学生信息（id必选，要改什么传什么）
+     * @param updateStudentByEmpDTO
+     * @param httpServletRequest
+     * @return
+     */
 
     @PutMapping("/updateStudent")
     public BaseResponse updateStudent(@RequestBody UpdateStudentByEmpDTO updateStudentByEmpDTO, HttpServletRequest httpServletRequest){
         return employeeService.updateStudent(updateStudentByEmpDTO,httpServletRequest);
     }
 
+    /**
+     * 学生处管理：查询学生，分页查询
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+
     @GetMapping("/queryStudentList")
     public BaseResponse queryStudentList(Integer pageNum, Integer pageSize){
         return employeeService.queryStudentList(pageNum,pageSize);
     }
+
+    /**
+     * 退出登录（教职工）
+     * @param httpServletRequest
+     * @return
+     */
 
 
     @PostMapping("/logout")
     public BaseResponse logout(HttpServletRequest httpServletRequest){
         return employeeService.logout(httpServletRequest);
     }
+
+    /**
+     * 删除教职工或者学生，根据id号不同(学生处管理员权限)
+     * @param id
+     * @param httpServletRequest
+     * @return
+     */
 
     @DeleteMapping("/deleteUser")
     public BaseResponse deleteUser(Integer id,HttpServletRequest httpServletRequest){
--- src/main/java/com/example/controller/StudentController.java
+++ src/main/java/com/example/controller/StudentController.java
@@ -16,25 +16,40 @@ import javax.servlet.http.HttpServletRequest;
 public class StudentController {
     @Autowired
     StudentService studentService;
+
+    /**学生注册
+
+     */
     @PostMapping("/register")
     public BaseResponse register(@RequestBody RegisterStudentDTO registerStudentDTO){
         return studentService.register(registerStudentDTO );
     }
+    /**
+    学生登录
+     */
     @PostMapping("/login")
     public BaseResponse login(@RequestBody LoginStudentDTO loginStudentDTO){
         return studentService.login(loginStudentDTO );
     }
+    /**
+
+     学生修改个人信息
+     */
 
     @PutMapping("/updateInfo")
     public BaseResponse updateInfo(@RequestBody UpdateStudentDTO updateStudentDTO, HttpServletRequest httpServletRequest){
         return studentService.updateInfo(updateStudentDTO,httpServletRequest);
     }
 
+    /**
+     * 学生查询个人信息
+
+     */
     @GetMapping("/queryInfo")
     public BaseResponse queryInfo(HttpServletRequest httpServletRequest){
         return studentService.queryInfo(httpServletRequest);
     }
-    /*
+    /**
     找回密码
      */
     @PutMapping("/findPassword")
@@ -42,6 +57,10 @@ public class StudentController {
         return studentService.findPassword(findPasswordDTO);
     }
 
+    /**
+
+     退出登录
+     */
 
     @PostMapping("/logout")
     public BaseResponse logout(HttpServletRequest httpServletRequest){
